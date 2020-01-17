899acf424be628e7d35dae98c12e6f2ace454136
hongyinghui <123997654@qq.com>
2020-01-17 19:10:39 +0800

    spring boot admin server and client use register server

--- spring-boot-admin-client/src/main/java/com/example/adminclient/AdminClientApplication.java
+++ spring-boot-admin-client/src/main/java/com/example/adminclient/AdminClientApplication.java
@@ -2,12 +2,14 @@ package com.example.adminclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author yinghuihong
  * @date 2020/1/17
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminClientApplication {
 
     public static void main(String[] args) {
